23a60f83febb7f130b4b2f62240470c91d59db67
Derek Snow <derek.snow@gmail.com>
2019-07-15 10:50:33 -0500

    first commit

---
+++ config.el
@@ -0,0 +1,14 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+
+;; Disable evil-snipe so that s key works as standard vim substition
+;; (after! evil-snipe
+;;   (evil-snipe-mode -1))
+
+;;;;;;;;;;;;;;;;
+;; Org Config ;;
+;;;;;;;;;;;;;;;;
+;; Insert time when "todo" marked "done"
+(setq org-log-done t)
